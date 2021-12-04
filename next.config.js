b2d0f41db51b3420bd571133b7439ba40bd7b6ff
setargyoom <81277009+setargyoom@users.noreply.github.com>
2021-12-04 23:39:42 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
+import Image from 'next/image'
+import profilePic from '../public/me.png'
+
+function Home() {
+  return (
+    <>
+      <h1>My Homepage</h1>
+      <Image
+        src={profilePic}
+        alt="Picture of the author"
+        // width={500} automatically provided
+        // height={500} automatically provided
+        // blurDataURL="data:..." automatically provided
+        // placeholder="blur" // Optional blur-up while loading
+      />
+      <p>Welcome to my homepage!</p>
+    </>
+  )
+}
+
+
 const sourcebit = require('sourcebit');
 
 const sourcebitConfig = require('./sourcebit.js');
