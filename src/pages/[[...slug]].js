11da3bbc0dabffae02638d88c2e0badf753a05e0
setargyoom <81277009+setargyoom@users.noreply.github.com>
2021-12-04 23:44:55 +0700

    Update [[...slug]].js

--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -5,26 +5,6 @@ import { withRemoteDataUpdates } from 'sourcebit-target-next/with-remote-data-up
 
 import pageLayouts from '../layouts';
 
-import Image from 'next/image'
-import profilePic from '../public/me.png'
-
-function Home() {
-  return (
-    <>
-      <h1>My Homepage</h1>
-      <Image
-        src={profilePic}
-        alt="Picture of the author"
-        // width={500} automatically provided
-        // height={500} automatically provided
-        // blurDataURL="data:..." automatically provided
-        // placeholder="blur" // Optional blur-up while loading
-      />
-      <p>Welcome to my homepage!</p>
-    </>
-  )
-}
-
 class Page extends React.Component {
     render() {
         const modelName = _.get(this.props, 'page.__metadata.modelName');
