9d7f44d4a2162710f15b9b53b8c544727866a18a
setargyoom <81277009+setargyoom@users.noreply.github.com>
2021-12-06 10:30:19 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,21 @@
-import Image from 'next/image'
-import profilePic from '../public/me.png'
+const sourcebit = require('sourcebit');
 
-function Home() {
-  return (
-    <>
-      <h1>My Homepage</h1>
-      <Image
-        src={profilePic}
-        alt="Picture of the author"
-        // width={500} automatically provided
-        // height={500} automatically provided
-        // blurDataURL="data:..." automatically provided
-        // placeholder="blur" // Optional blur-up while loading
-      />
-      <p>Welcome to my homepage!</p>
-    </>
-  )
-}
+const sourcebitConfig = require('./sourcebit.js');
+
+sourcebit.fetch(sourcebitConfig);
+
+module.exports = {
+    trailingSlash: true,
+    devIndicators: {
+        autoPrerender: false
+    },
+    webpack: (config, { webpack }) => {
+        // Tell webpack to ignore watching content files in the content folder.
+        // Otherwise webpack receompiles the app and refreshes the whole page.
+        // Instead, the src/pages/[...slug].js uses the "withRemoteDataUpdates"
+        // function to update the content on the page without refreshing the
+        // whole page
+        config.plugins.push(new webpack.WatchIgnorePlugin([[/\/content\//]]));
+        return config;
+    }
+};
